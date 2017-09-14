4a4413be69974147dcb32167802485d532d21166
Junhao Li <streaver91@gmail.com>
2017-09-14 14:48:58 -0400

    factor out hash and lock

--- src/omp_hash_map.h
+++ src/omp_hash_map.h
@@ -89,9 +89,12 @@ class omp_hash_map {
 
   std::vector<std::unique_ptr<hash_node>> buckets;
 
-  // Apply node_handler to the node if it has the specific key or is a null pointer.
-  // Otherwise, call itself recursively with the next pointer.
+  // Apply node_handler to the hash node which either has the specific key or is a null pointer.
   void hash_node_apply(
+      const K& key, const std::function<void(std::unique_ptr<hash_node>&)>& node_handler);
+
+  // Apply node_handler to the hash node which either has the specific key or is a null pointer.
+  void hash_node_apply_recursive(
       std::unique_ptr<hash_node>& node,
       const K& key,
       const std::function<void(std::unique_ptr<hash_node>&)>& node_handler);
@@ -117,8 +120,6 @@ omp_hash_map<K, V, H>::~omp_hash_map() {
 
 template <class K, class V, class H>
 void omp_hash_map<K, V, H>::set(const K& key, const V& value) {
-  const size_t hash_value = hasher(key);
-  const size_t segment_id = hash_value % n_segments;
   const auto& node_handler = [&](std::unique_ptr<hash_node>& node) {
     if (!node) {
       node.reset(new hash_node(key, value));
@@ -126,29 +127,31 @@ void omp_hash_map<K, V, H>::set(const K& key, const V& value) {
       node->value = value;
     }
   };
-  auto& lock = segment_locks[segment_id];
-  omp_set_lock(&lock);
-  const size_t bucket_id = hash_value % n_buckets;
-  hash_node_apply(buckets[bucket_id], key, node_handler);
-  omp_unset_lock(&lock);
+  hash_node_apply(key, node_handler);
 }
 
 template <class K, class V, class H>
 void omp_hash_map<K, V, H>::apply(const K& key, const std::function<void(const V&)>& handler) {
-  const size_t hash_value = hasher(key);
-  const size_t segment_id = hash_value % n_segments;
   const auto& node_handler = [&](std::unique_ptr<hash_node>& node) {
     if (node) handler(node->value);
   };
-  auto& lock = segment_locks[segment_id];
-  omp_set_lock(&lock);
-  const size_t bucket_id = hash_value % n_buckets;
-  hash_node_apply(buckets[bucket_id], key, node_handler);
-  omp_unset_lock(&lock);
+  hash_node_apply(key, node_handler);
 }
 
 template <class K, class V, class H>
 void omp_hash_map<K, V, H>::hash_node_apply(
+    const K& key, const std::function<void(std::unique_ptr<hash_node>&)>& node_handler) {
+  const size_t hash_value = hasher(key);
+  const size_t segment_id = hash_value % n_segments;
+  auto& lock = segment_locks[segment_id];
+  omp_set_lock(&lock);
+  const size_t bucket_id = hash_value % n_buckets;
+  hash_node_apply_recursive(buckets[bucket_id], key, node_handler);
+  omp_unset_lock(&lock);
+}
+
+template <class K, class V, class H>
+void omp_hash_map<K, V, H>::hash_node_apply_recursive(
     std::unique_ptr<hash_node>& node,
     const K& key,
     const std::function<void(std::unique_ptr<hash_node>&)>& node_handler) {
@@ -156,7 +159,7 @@ void omp_hash_map<K, V, H>::hash_node_apply(
     if (node->key == key) {
       node_handler(node);
     } else {
-      hash_node_apply(node->next, key, node_handler);
+      hash_node_apply_recursive(node->next, key, node_handler);
     }
   } else {
     node_handler(node);
